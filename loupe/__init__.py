a9aa51e346d8c4caa84d980de53058a2d016f652
Corey Oordt <coreyoordt@gmail.com>
2013-05-01 13:52:07 -0500

    Version bump: 0.3.2

--- loupe/__init__.py
+++ loupe/__init__.py
@@ -5,7 +5,7 @@ A high resolution image manager for pan and zooming into images
 __version_info__ = {
     'major': 0,
     'minor': 3,
-    'micro': 1,
+    'micro': 2,
     'releaselevel': 'final',
     'serial': 1
 }
